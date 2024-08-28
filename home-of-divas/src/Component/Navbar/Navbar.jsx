0fc2e623e036f1e22a1f0b586d98b04a96c24106
Empire688682 <asehindej@gmail.com>
2024-08-28 18:41:43 +0100

    menubar

--- home-of-divas/src/Component/Navbar/Navbar.jsx
+++ home-of-divas/src/Component/Navbar/Navbar.jsx
@@ -1,34 +1,79 @@
 "use client";
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Navbar.module.css';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { FaUserCircle } from "react-icons/fa";
 import { FiShoppingCart } from "react-icons/fi";
+import { FaHeart } from "react-icons/fa";
+import Image from 'next/image';
+import { LiaTimesSolid } from "react-icons/lia";
 
 const Navbar = () => {
   const pathname = usePathname();
+  const [showMenu, setShowMenu] = useState(false);
 
   return (
     <div className={styles.navbar}>
       <div className={styles.logo}>
         <h3>Divas</h3>
       </div>
-      <ul className={styles.menu}>
-        <Link href="/" className={`${styles.links} ${pathname === '/' ? styles.active : ''}`}>Home</Link>
-        <Link href="/about" className={`${styles.links} ${pathname === '/about' ? styles.active : ''}`}>About</Link>
-        <Link href="/project" className={`${styles.links} ${pathname === '/project' ? styles.active : ''}`}>Project</Link>
-        <Link href="/shop" className={`${styles.links} ${pathname === '/shop' ? styles.active : ''}`}>Shop</Link>
-        <Link href="/contact" className={`${styles.links} ${pathname === '/contact' ? styles.active : ''}`}>Contact</Link>
-      </ul>
-      <div className={styles.user_login_cart}>
-        <div className={styles.cart}>
-          <FiShoppingCart />
-        </div>
-        <div className={styles.user_login}>
-          <FaUserCircle />
+      <div className={styles.big_screen_menu}>
+        <ul className={styles.menu}>
+          <Link href="/" className={`${styles.links} ${pathname === '/' ? styles.active : ''}`}>Home</Link>
+          <Link href="/about" className={`${styles.links} ${pathname === '/about' ? styles.active : ''}`}>About</Link>
+          <Link href="/project" className={`${styles.links} ${pathname === '/project' ? styles.active : ''}`}>Project</Link>
+          <Link href="/service" className={`${styles.links} ${pathname === '/service' ? styles.active : ''}`}>Service</Link>
+          <Link href="/shop" className={`${styles.links} ${pathname === '/shop' ? styles.active : ''}`}>Shop</Link>
+          <Link href="/contact" className={`${styles.links} ${pathname === '/contact' ? styles.active : ''}`}>Contact</Link>
+        </ul>
+        <div className={styles.user_login_cart}>
+          <div className={styles.cart}>
+            <FiShoppingCart />
+          </div>
+          <div className={styles.user_login}>
+            <FaUserCircle />
+          </div>
+          <div className={styles.user_fav}>
+            <FaHeart />
+          </div>
         </div>
       </div>
+      <div onClick={()=>setShowMenu(!showMenu)} className={styles.menu_bar}>
+        <Image src="/dark_menu.png" alt="Menu Icon" width={50} height={50} />
+      </div>
+
+      {
+        //MOBILE MENU
+        <div className={`${styles.small_screen_menu} ${!showMenu? styles.active:""}`}>
+          <div onClick={()=>setShowMenu(false)} className={styles.menu_close}>
+            <LiaTimesSolid />
+          </div>
+          <div className={styles.logo}>
+            <h3>Divas</h3>
+          </div>
+          <div className={styles.user_login_cart}>
+            <div className={styles.cart}>
+              <FiShoppingCart />
+            </div>
+            <div className={styles.user_login}>
+              <FaUserCircle />
+            </div>
+            <div className={styles.user_fav}>
+              <FaHeart />
+            </div>
+          </div>
+
+          <ul className={styles.menu}>
+            <Link href="/" className={`${styles.links} ${pathname === '/' ? styles.active : ''}`}>Home</Link>
+            <Link href="/about" className={`${styles.links} ${pathname === '/about' ? styles.active : ''}`}>About</Link>
+            <Link href="/project" className={`${styles.links} ${pathname === '/project' ? styles.active : ''}`}>Project</Link>
+            <Link href="/service" className={`${styles.links} ${pathname === '/service' ? styles.active : ''}`}>Service</Link>
+            <Link href="/shop" className={`${styles.links} ${pathname === '/shop' ? styles.active : ''}`}>Shop</Link>
+            <Link href="/contact" className={`${styles.links} ${pathname === '/contact' ? styles.active : ''}`}>Contact</Link>
+          </ul>
+        </div>
+      }
     </div>
   );
 };
