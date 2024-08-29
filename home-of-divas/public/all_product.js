977afceb6e60a0dc47c9a7bd81aae309ed0383a8
Empire688682 <asehindej@gmail.com>
2024-08-29 21:43:26 +0100

    shop

--- home-of-divas/public/all_product.js
+++ home-of-divas/public/all_product.js
@@ -38,7 +38,7 @@ import p36_img from "./product_36.png";
 let all_product = [
   {
     id: 1,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Striped Flutter Sleeve",
     category: "Women",
     image: p1_img,
     new_price: 5000,
@@ -46,7 +46,7 @@ let all_product = [
   },
   {
     id: 2,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Striped Flutter Sleeve",
     category: "Women",
     image: p2_img,
     new_price: 8500,
@@ -54,7 +54,7 @@ let all_product = [
   },
   {
     id: 3,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Striped Flutter Sleeve",
     category: "Women",
     image: p3_img,
     new_price: 6000,
@@ -62,7 +62,7 @@ let all_product = [
   },
   {
     id: 4,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Striped Flutter Sleeve",
     category: "Women",
     image: p4_img,
     new_price: 10000,
@@ -70,7 +70,7 @@ let all_product = [
   },
   {
     id: 5,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Striped Flutter Sleeve",
     category: "Women",
     image: p5_img,
     new_price: 8500,
@@ -78,7 +78,7 @@ let all_product = [
   },
   {
     id: 6,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Striped Flutter Sleeve",
     category: "Women",
     image: p6_img,
     new_price: 8500,
@@ -86,7 +86,7 @@ let all_product = [
   },
   {
     id: 7,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Striped Flutter Sleeve",
     category: "Women",
     image: p7_img,
     new_price: 8500,
@@ -94,7 +94,7 @@ let all_product = [
   },
   {
     id: 8,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Striped Flutter Sleeve",
     category: "Women",
     image: p8_img,
     new_price: 8500,
@@ -102,7 +102,7 @@ let all_product = [
   },
   {
     id: 9,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Striped Flutter Sleeve",
     category: "Women",
     image: p9_img,
     new_price: 8500,
@@ -110,7 +110,7 @@ let all_product = [
   },
   {
     id: 10,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Striped Flutter Sleeve",
     category: "Women",
     image: p10_img,
     new_price: 8500,
@@ -118,7 +118,7 @@ let all_product = [
   },
   {
     id: 11,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Striped Flutter Sleeve",
     category: "Women",
     image: p11_img,
     new_price: 8500,
@@ -126,7 +126,7 @@ let all_product = [
   },
   {
     id: 12,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Striped Flutter Sleeve",
     category: "Women",
     image: p12_img,
     new_price: 8500,
@@ -134,7 +134,7 @@ let all_product = [
   },
   {
     id: 13,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men Zippered Jacket",
     category: "Men",
     image: p13_img,
     new_price: 8500,
@@ -142,7 +142,7 @@ let all_product = [
   },
   {
     id: 14,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men Zippered Jacket",
     category: "Men",
     image: p14_img,
     new_price: 8500,
@@ -150,7 +150,7 @@ let all_product = [
   },
   {
     id: 15,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men Zippered Jacket",
     category: "Men",
     image: p15_img,
     new_price: 8500,
@@ -158,7 +158,7 @@ let all_product = [
   },
   {
     id: 16,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men Zippered Jacket",
     category: "Men",
     image: p16_img,
     new_price: 8500,
@@ -166,7 +166,7 @@ let all_product = [
   },
   {
     id: 17,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men Zippered Jacket",
     category: "Men",
     image: p17_img,
     new_price: 8500,
@@ -174,7 +174,7 @@ let all_product = [
   },
   {
     id: 18,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men Zippered Jacket",
     category: "Men",
     image: p18_img,
     new_price: 8500,
@@ -182,7 +182,7 @@ let all_product = [
   },
   {
     id: 19,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men Zippered Jacket",
     category: "Men",
     image: p19_img,
     new_price: 8500,
@@ -190,7 +190,7 @@ let all_product = [
   },
   {
     id: 20,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men Zippered Jacket",
     category: "Men",
     image: p20_img,
     new_price: 8500,
@@ -198,7 +198,7 @@ let all_product = [
   },
   {
     id: 21,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men Zippered Jacket",
     category: "Men",
     image: p21_img,
     new_price: 8500,
@@ -206,7 +206,7 @@ let all_product = [
   },
   {
     id: 22,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men Zippered Jacket",
     category: "Men",
     image: p22_img,
     new_price: 8500,
@@ -214,7 +214,7 @@ let all_product = [
   },
   {
     id: 23,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men Zippered Jacket",
     category: "Men",
     image: p23_img,
     new_price: 8500,
@@ -222,7 +222,7 @@ let all_product = [
   },
   {
     id: 24,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    name: "Men Zippered Jacket",
     category: "Men",
     image: p24_img,
     new_price: 8500,
@@ -230,7 +230,7 @@ let all_product = [
   },
   {
     id: 25,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Boys Hooded Sweatshirt",
     category: "Kids",
     image: p25_img,
     new_price: 8500,
@@ -238,7 +238,7 @@ let all_product = [
   },
   {
     id: 26,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Boys Hooded Sweatshirt",
     category: "Kids",
     image: p26_img,
     new_price: 8500,
@@ -246,7 +246,7 @@ let all_product = [
   },
   {
     id: 27,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Boys Hooded Sweatshirt",
     category: "Kids",
     image: p27_img,
     new_price: 8500,
@@ -254,7 +254,7 @@ let all_product = [
   },
   {
     id: 28,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Boys Hooded Sweatshirt",
     category: "Kids",
     image: p28_img,
     new_price: 8500,
@@ -262,7 +262,7 @@ let all_product = [
   },
   {
     id: 29,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Boys Hooded Sweatshirt",
     category: "Kids",
     image: p29_img,
     new_price: 8500,
@@ -270,7 +270,7 @@ let all_product = [
   },
   {
     id: 30,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Boys Hooded Sweatshirt",
     category: "Kids",
     image: p30_img,
     new_price: 8500,
@@ -278,7 +278,7 @@ let all_product = [
   },
   {
     id: 31,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Boys Hooded Sweatshirt",
     category: "Kids",
     image: p31_img,
     new_price: 8500,
@@ -286,7 +286,7 @@ let all_product = [
   },
   {
     id: 32,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Boys Hooded Sweatshirt",
     category: "Kids",
     image: p32_img,
     new_price: 8500,
@@ -294,7 +294,7 @@ let all_product = [
   },
   {
     id: 33,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Boys Hooded Sweatshirt",
     category: "Kids",
     image: p33_img,
     new_price: 8500,
@@ -302,7 +302,7 @@ let all_product = [
   },
   {
     id: 34,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Boys Hooded Sweatshirt",
     category: "Kids",
     image: p34_img,
     new_price: 8500,
@@ -310,7 +310,7 @@ let all_product = [
   },
   {
     id: 35,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Boys Hooded Sweatshirt",
     category: "Kids",
     image: p35_img,
     new_price: 8500,
@@ -318,7 +318,7 @@ let all_product = [
   },
   {
     id: 36,
-    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    name: "Boys Hooded Sweatshirt",
     category: "Kids",
     image: p36_img,
     new_price: 8500,
--- home-of-divas/src/Component/ShopProduct/ShopProduct.jsx
+++ home-of-divas/src/Component/ShopProduct/ShopProduct.jsx
@@ -3,32 +3,62 @@ import React, { useState } from 'react';
 import styles from './ShopProduct.module.css';
 import Image from 'next/image';
 import all_product from '../../../public/all_product';
+import { FaStar } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+import { FiShoppingCart } from "react-icons/fi";
 
 const ShopProduct = () => {
     const [category, setCategory] = useState("All");
-  return (
-    <div className={styles.shop_product}>
-      <div className={styles.shop_header}>
-        <h2>Popular on Divas</h2>
-        <div className={styles.shop_menus}>
-          <li className={`${category === "All"?styles.active:""}`} onClick={()=>setCategory("All")}>All <b>/</b> </li>
-          <li className={`${category === "Women"?styles.active:""}`}  onClick={()=>setCategory((prev)=> prev !== "Women"? "Women":"All")}>Women <b>/</b> </li>
-          <li className={`${category === "Men"?styles.active:""}`}  onClick={()=>setCategory((prev)=> prev !== "Men"? "Men":"All")}>Men <b>/</b> </li>
-          <li className={`${category === "Kids"?styles.active:""}`}  onClick={()=>setCategory((prev)=> prev !== "Kids"? "Kids":"All")}>Kids <b>/</b></li>
-          <li className={`${category === "Materials"?styles.active:""}`}  onClick={()=>setCategory((prev)=> prev !== "Materials"? "Materials":"All")}>Materials</li>
-        </div>
-      </div>
-      {
-        all_product.map((item)=>{
-            if(category === "All" || item.category === category){
-                return <div>
-                    <div>{item.name}</div>
+    return (
+        <div className={styles.shop_product}>
+            <div className={styles.shop_header}>
+                <h2>Popular on Divas</h2>
+                <div className={styles.shop_menus}>
+                    <li className={`${category === "All" ? styles.active : ""}`} onClick={() => setCategory("All")}>All <b>/</b> </li>
+                    <li className={`${category === "Women" ? styles.active : ""}`} onClick={() => setCategory((prev) => prev !== "Women" ? "Women" : "All")}>Women <b>/</b> </li>
+                    <li className={`${category === "Men" ? styles.active : ""}`} onClick={() => setCategory((prev) => prev !== "Men" ? "Men" : "All")}>Men <b>/</b> </li>
+                    <li className={`${category === "Kids" ? styles.active : ""}`} onClick={() => setCategory((prev) => prev !== "Kids" ? "Kids" : "All")}>Kids <b>/</b></li>
+                    <li className={`${category === "Materials" ? styles.active : ""}`} onClick={() => setCategory((prev) => prev !== "Materials" ? "Materials" : "All")}>Materials</li>
                 </div>
+            </div>
+            <div className={styles.shop_items}>
+            {
+                all_product.map((item, i) => {
+                    if (category === "All" || item.category === category) {
+                        return <div key={i} className={styles.shop_cart}>
+                            <div className={styles.img_com}>
+                                <Image src={item.image} alt="" fill />
+                            </div>
+                            <div className={styles.item_text}>
+                                <div className={styles.item_name}>
+                                    {item.name}
+                                </div>
+                                <div className={styles.item_price}>
+                                    <div className={styles.item_stars}>
+                                    <FaStar className={styles.star} />
+                                    <FaStar className={styles.star} />
+                                    <FaStar className={styles.star} />
+                                    <FaStar className={styles.star} />
+                                    <FaStar className={styles.star} />
+                                    </div>
+                                    <p>#{item.new_price}</p>
+                                </div>
+                            </div>
+                            <div className={styles.cart_fav_Con}>
+                                <div className={styles.fav}>
+                                    <FaHeart/>
+                                </div>
+                                <div className={styles.cart}>
+                                    <FiShoppingCart/>
+                                </div>
+                            </div>
+                        </div>
+                    }
+                })
             }
-        })
-      }
-    </div>
-  )
+            </div>
+        </div>
+    )
 }
 
 export default ShopProduct
