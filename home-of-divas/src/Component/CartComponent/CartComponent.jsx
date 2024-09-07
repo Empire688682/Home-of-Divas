b94f82c499c49157d823c46e0bbbaf8362465687
Empire688682 <asehindej@gmail.com>
2024-09-07 20:48:07 +0100

    cart

--- home-of-divas/src/Component/CartComponent/CartComponent.jsx
+++ home-of-divas/src/Component/CartComponent/CartComponent.jsx
@@ -1,28 +1,54 @@
-"use client"; // This directive tells Next.js to render this component on the client side
+"use client"; 
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import styles from './CartComponent.module.css';
+import Image from 'next/image';
 
-import React from 'react';
-import Link from 'next/link'; // Import the Link component from Next.js
-import styles from './CartComponent.module.css'; // Make sure the module name matches (singular "module")
-
-import { useGlobalContext } from '../Context'; // Adjust this path if necessary
+import { useGlobalContext } from '../Context'; 
 
 const CartComponent = () => {
-  const { allProduct, cartItems, removeFromCart, addToCart, getTotalValue } = useGlobalContext();
-  
+  const { allProduct, cartItems, removeFromCart, addToCart, inCart, getTotalValue } = useGlobalContext();
+  const [availableItems, setAvailableItems] = useState(0);
+
+const handleAvailableItems = () => {
+    let itemCount = 0;
+    allProduct.forEach((item)=>{
+        if(cartItems[item.id] > 0){
+            itemCount += 1
+        }
+        return itemCount
+    })
+  setAvailableItems(itemCount);
+};
+
+useEffect(() => {
+  handleAvailableItems();
+}, [allProduct, cartItems]);
+ 
   return (
-    <div className={styles.Cart}> {/* Use the imported styles */}
-      <div className={styles.cartHead}>
-        <h1 className={styles.product}>Product</h1>
-        <h1 className={styles.headTitle}>Price</h1>
-        <h1 className={styles.headTitle}>Quantity</h1>
-        <h1 className={styles.headTitle}>Total</h1>
-      </div>
+    <div className={styles.Cart}>
+        <h1>Shopping Cart ({availableItems} item) </h1>
+      {
+        inCart? <div className={styles.cartHead}>
+        <h2 className={styles.product}>Product</h2>
+        <h2 className={styles.headTitle}>Price</h2>
+        <h2 className={styles.headTitle}>Quantity</h2>
+        <h2 className={styles.headTitle}>Total</h2>
+      </div>:<p>Nothing in Cart</p>
+      }
       {allProduct.map((item) => {
         if (cartItems[item.id] > 0) {
           return (
-            <div className={`${styles.cartHead} ${styles.cartItems}`} key={item._id}>
-              <p className={styles.product}>{item.name}</p>
-              <p className={styles.headTitle}>#{item.new_price}</p>
+            <div className={` ${styles.cartItems}`} key={item.id}>
+              <div className={styles.product}>
+              <div className={styles.img_Con}>
+                <Image src={item.image} alt='' fill/>
+              </div>
+              <p>{item.name}</p>
+              </div>
+              <div className={styles.headTitle}>
+                #{item.new_price}
+                </div>
               <div className={`${styles.headTitle} ${styles.quantityCon}`}>
                 <h3 className={styles.quantity}>{cartItems[item.id]}</h3>
                 <div className={styles.plusMinus}>
@@ -30,26 +56,32 @@ const CartComponent = () => {
                   <p onClick={() => removeFromCart(item.id)}>-</p>
                 </div>
               </div>
-              <p className={styles.headTitle}>#{item.new_price * cartItems[item.id]}</p>
+              <p className={styles.headTitle}>
+                #{item.new_price * cartItems[item.id]}
+                </p>
             </div>
           );
         } else {
           return null;
         }
       })}
-      <div className={`${styles.cartHead} ${styles.cartItems}`}>
+     {
+        inCart?  <div className={`${styles.cartHead}`}>
         <p className={styles.product}></p>
         <p className={styles.headTitle}></p>
-        <div className={`${styles.headTitle} ${styles.quantityCon}`}>
+        <div className={`${styles.subTotal} ${styles.quantityCon}`}>
           <h3 className={styles.plusMinus}>
             Sub Total
           </h3>
         </div>
-        <h3 className={styles.headTitle}>TOTALVALUE</h3> {/* Assuming getTotalValue calculates total */}
-      </div>
-      <div className={styles.checkOutBtnCon}>
+        <h3>TOTALVALUE</h3> {/* Assuming getTotalValue calculates total */}
+      </div>:null
+     }
+      {
+        inCart? <div className={styles.checkOutBtnCon}>
         <Link href="/order" className={styles.checkOutBtn}>Check Out</Link>
-      </div>
+      </div>:null
+      }
     </div>
   );
 };
