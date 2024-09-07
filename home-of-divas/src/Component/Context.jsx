9d7779abb03e70a6d86530a831a6f186f4da9998
Empire688682 <asehindej@gmail.com>
2024-09-07 01:56:33 +0100

    add and remove from cart

--- home-of-divas/src/Component/Context.jsx
+++ home-of-divas/src/Component/Context.jsx
@@ -3,51 +3,68 @@ import React, { useContext, useEffect, useState } from 'react';
 
 const GlobalContext = React.createContext();
 
-export const GlobalProvider = ({children}) => {
+export const GlobalProvider = ({ children }) => {
 
-    const [cartItems, setCartItems] = useState({});
-    const [token, setToken] = useState('');
-    const [user, setUser] = useState('');
+  const [cartItems, setCartItems] = useState({});
+  const [token, setToken] = useState('');
+  const [user, setUser] = useState('');
+  const [itemAdded, setItemAdded] = useState(null);
 
-    useEffect(()=>{
-      if(typeof window !== "undefined"){
-        const savedToken = localStorage.getItem("Divastoken") || "";
-        const savedUser = localStorage.getItem("Divasuserdata") || "";
-        setToken(savedToken);
-        setUser(savedUser);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const savedToken = localStorage.getItem("Divastoken") || "";
+      const savedUser = localStorage.getItem("Divasuserdata") || "";
+      setToken(savedToken);
+      setUser(savedUser);
+    }
+  }, []);
+
+  const addToCart = (itemId) => {
+    setItemAdded("true");
+    setTimeout(()=>{
+      setItemAdded(null)
+    },1000)
+    setCartItems((prev) => {
+      if (!prev[itemId]) {
+        return { ...prev, [itemId]: 1 };
       }
-    },[]);
+      else {
+        return { ...prev, [itemId]: prev[itemId] + 1 };
+      };
+    });
+  };
 
-    const addToCart = (itemId) =>{
-      if(cartItems[itemId] < 0){
-        setCartItems((prev) =>{
-          if(!prev[itemId]){
-            return {...prev, [itemId]:1}
-          }
-          else{
-            return {...prev, [itemId]: +1}
-          }
-        })
-      }
-    };
+  const removeFromCart = (itemId) => {
+    setItemAdded("false");
+    setTimeout(()=>{
+      setItemAdded(null)
+    },1000)
+    if (cartItems[itemId] > 0) {
+      setCartItems((prev) => {
+        return ({ ...prev, [itemId]: prev[itemId] - 1 });
+      })
+    } else return null
+  };
 
-    console.log("CARTITEMS:", cartItems);
+  console.log("CARTITEMS:", cartItems);
 
   return (
     <GlobalContext.Provider value={{
-        cartItems,
-        token, 
-        setToken,
-        user,
-        setUser,
-        addToCart
+      cartItems,
+      token,
+      setToken,
+      user,
+      setUser,
+      addToCart,
+      removeFromCart,
+      itemAdded
     }}>
-        {children}
+      {children}
     </GlobalContext.Provider>
   )
 }
 
-export const useGlobalContext = () =>{
-    return useContext(GlobalContext);
+export const useGlobalContext = () => {
+  return useContext(GlobalContext);
 }
 
--- home-of-divas/src/Component/ShopProduct/ShopProduct.jsx
+++ home-of-divas/src/Component/ShopProduct/ShopProduct.jsx
@@ -10,7 +10,7 @@ import { useGlobalContext } from '../Context';
 
 const ShopProduct = () => {
     const [category, setCategory] = useState("All");
-    const {addToCart} = useGlobalContext()
+    const { addToCart, removeFromCart, itemAdded, cartItems } = useGlobalContext();
     return (
         <div className={styles.shop_product}>
             <div className={styles.shop_header}>
@@ -24,41 +24,59 @@ const ShopProduct = () => {
                 </div>
             </div>
             <div className={styles.shop_items}>
-            {
-                all_product.map((item, i) => {
-                    if (category === "All" || item.category === category) {
-                        return <div key={i} className={styles.shop_cart}>
-                            <div className={styles.img_com}>
-                                <Image src={item.image} alt="" fill />
-                            </div>
-                            <div className={styles.item_text}>
-                                <div className={styles.item_name}>
-                                    {item.name}
+                {
+                    all_product.map((item) => {
+                        if (category === "All" || item.category === category) {
+                            return <div key={item.id} className={styles.shop_cart}>
+                                <div className={styles.img_com}>
+                                    <Image src={item.image} alt="" fill />
                                 </div>
-                                <div className={styles.item_price}>
-                                    <div className={styles.item_stars}>
-                                    <FaStar className={styles.star} />
-                                    <FaStar className={styles.star} />
-                                    <FaStar className={styles.star} />
-                                    <FaStar className={styles.star} />
-                                    <FaStar className={styles.star} />
+                                <div className={styles.item_text}>
+                                    <div className={styles.item_name}>
+                                        {item.name}
                                     </div>
-                                    <p>#{item.new_price}</p>
+                                    <div className={styles.item_price}>
+                                        <div className={styles.item_stars}>
+                                            <FaStar className={styles.star} />
+                                            <FaStar className={styles.star} />
+                                            <FaStar className={styles.star} />
+                                            <FaStar className={styles.star} />
+                                            <FaStar className={styles.star} />
+                                        </div>
+                                        <p>#{item.new_price}</p>
+                                    </div>
+                                </div>
+                                <div className={styles.cart_fav_Con}>
+                                    <div className={styles.fav}>
+                                        <FaHeart />
+                                    </div>
+                                    {
+                                        cartItems[item.id] > 0 ? <div className={styles.items_toggle_Con}>
+                                            <p className={styles.add_icon} onClick={() => addToCart(item.id)}>+</p>
+                                            <p>{cartItems[item.id]}</p>
+                                            <p className={styles.remove_icon} onClick={() => removeFromCart(item.id)}>-</p>
+                                        </div>
+                                        :
+                                        <div onClick={() => addToCart(item.id)} className={styles.cart}>
+                                        <FiShoppingCart />
+                                    </div>
+                                    }
                                 </div>
                             </div>
-                            <div className={styles.cart_fav_Con}>
-                                <div className={styles.fav}>
-                                    <FaHeart/>
-                                </div>
-                                <div onClick={()=> addToCart(item.id)} className={styles.cart}>
-                                    <FiShoppingCart/>
-                                </div>
-                            </div>
-                        </div>
-                    }
-                })
-            }
+                        }
+                    })
+                }
             </div>
+            {
+                itemAdded === "true" ? <div className={styles.item_add_message}>
+                    <p>Added To Cart Successfully!!</p>
+                </div>:null
+            }
+            {
+                itemAdded ==="false" ? <div className={styles.item_remove_message}>
+                    <p>Removed From Cart Successfully!!</p>
+                </div>:null
+            }
         </div>
     )
 }
