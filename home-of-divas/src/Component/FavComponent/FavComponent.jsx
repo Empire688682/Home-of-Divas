2ab698ccc69d607863a672b5bc74608c19326e17
Empire688682 <asehindej@gmail.com>
2024-09-09 13:42:49 +0100

    asap

--- home-of-divas/src/Component/FavComponent/FavComponent.jsx
+++ home-of-divas/src/Component/FavComponent/FavComponent.jsx
@@ -1,7 +1,9 @@
 'use client'
 import React from 'react'
+import { useGlobalContext } from '../Context'
 
 const FavComponent = () => {
+    const {favItem,} = useGlobalContext();
   return (
     <div>
       
