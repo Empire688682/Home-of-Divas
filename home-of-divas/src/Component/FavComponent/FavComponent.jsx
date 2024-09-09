586d375fe38b349217d9b220d8932c5fea45d1d0
Empire688682 <asehindej@gmail.com>
2024-09-09 18:32:14 +0100

    fav complete

--- home-of-divas/src/Component/FavComponent/FavComponent.jsx
+++ home-of-divas/src/Component/FavComponent/FavComponent.jsx
@@ -6,11 +6,17 @@ import Image from 'next/image';
 import { RxCross2 } from "react-icons/rx";
 
 const FavComponent = () => {
-    const {favItem, allProduct} = useGlobalContext();
+    const {favItem, inFav, allProduct, addToCart, handleFav} = useGlobalContext();
+    const handleBuynow =(id)=>{
+      addToCart(id);
+      handleFav(id)
+    }
+
   return (
     <div className={styles.fav}>
       <h3>My Favorites</h3>
-      <div className={styles.fav_Con}>
+      {
+        inFav ? <div className={styles.fav_Con}>
         {
           allProduct.map((product) =>{
             if(favItem[product.id]> 0){
@@ -26,15 +32,16 @@ const FavComponent = () => {
                 </div>
                 <div className={styles.cart_bottom}>
                 <div className={styles.all_btn}>
-                    <button className={styles.buy_btn}>BUY NOW</button>
-                    <button className={styles.remove_btn}><RxCross2 className={styles.icon} /> REMOVE</button>
+                    <button onClick={()=>handleBuynow(product.id)} className={styles.buy_btn}>BUY NOW</button>
+                    <button onClick={()=>handleFav(product.id)} className={styles.remove_btn}><RxCross2 className={styles.icon} /> REMOVE</button>
                   </div>
                 </div>
               </div>
             }
           })
         }
-      </div>
+      </div>:<p>Noting in Favorite</p>
+      }
     </div>
   )
 }
