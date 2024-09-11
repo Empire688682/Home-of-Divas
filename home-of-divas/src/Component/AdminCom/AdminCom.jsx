e2f8a7d24a1b4670b447b164d0bb4b0c021f3adf
Empire688682 <asehindej@gmail.com>
2024-09-11 23:57:58 +0100

    front

--- home-of-divas/src/Component/AdminCom/AdminCom.jsx
+++ home-of-divas/src/Component/AdminCom/AdminCom.jsx
@@ -2,11 +2,18 @@
 import React from 'react';
 import styles from './AdminCom.module.css';
 import SideBar from './SideBar/SiseBar';
+import { FaUserCircle } from "react-icons/fa";
 
 const AdminCom = () => {
   return (
     <div className={styles.admin_Com}>
-      <h1>Divas Admin Panel</h1>
+      <div className={styles.header}>
+      <h1>Admin Panel</h1>
+      <div className={styles.profile}>
+      <FaUserCircle />
+      <p>Logout</p>
+      </div>
+      </div>
       <SideBar />
     </div>
   )
--- home-of-divas/src/Component/AdminCom/SideBar/SiseBar.jsx
+++ home-of-divas/src/Component/AdminCom/SideBar/SiseBar.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import styles from './SideBar.module.css'
 import { IoMdAdd } from "react-icons/io";
 import { BsBasket3 } from "react-icons/bs";
 import { MdChecklistRtl } from "react-icons/md";
 
 const SideBar = () => {
+    const [menu, setMenu] = useState("add");
+
   return (
     <div className={styles.side_Bar}>
-      
+      <div className={styles.side_Bar_Menu_Con}>
+        <div onClick={()=> setMenu("add")} className={menu === "add"? `${styles.side_Bar_Menu } ${styles.active}`:`${styles.side_Bar_Menu }`}>
+            <IoMdAdd className={styles.menu_Icon}/>
+            <p>Add Item</p>
+        </div>
+        <div onClick={()=> setMenu("list")} className={menu === "list"? `${styles.side_Bar_Menu } ${styles.active}`:`${styles.side_Bar_Menu }`}>
+            <MdChecklistRtl className={styles.menu_Icon}/>
+            <p>Item List</p>
+        </div>
+        <div onClick={()=> setMenu("order")} className={menu === "order"? `${styles.side_Bar_Menu } ${styles.active}`:`${styles.side_Bar_Menu }`}>
+            <BsBasket3 className={styles.menu_Icon}/>
+            <p>Order</p>
+        </div>
+      </div>
     </div>
   )
 }
