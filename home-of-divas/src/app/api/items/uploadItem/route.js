e98b4eb44c46ebdee62326b30f98b0c42b2f9eeb
Empire688682 <asehindej@gmail.com>
2024-09-12 23:56:20 +0100

    back

--- home-of-divas/src/app/api/items/uploadItem/route.js
+++ home-of-divas/src/app/api/items/uploadItem/route.js
@@ -1,97 +1,64 @@
-import { ItemModel } from "@/model/itemModel";
-import multer from "multer";
-import fs from 'fs';
-import { connectDB } from "@/ConnectDB/ConnectDB";
-import path, { resolve } from "path";
-import { rejects } from "assert";
-import { error } from "console";
+import multer from 'multer';
+import { ItemModel } from '@/model/itemModel';
+import path from 'path';
+import fs, { existsSync } from 'fs';
+import { connectDB } from '@/ConnectDB/ConnectDB';
 
-const upload = multer.diskStorage({
-    destination: "./public/uploads",
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: './public/uploads',
     filename: (req, file, cb) => {
-        return cb(null, `${Date.now()}_${file.originalname}`);
+     return cb(null, `${Date.now()}_${file.originalname}`);
     }
+  })
 });
 
 export const config = {
-    api: {
-        bodyParser: false
-    }
-}
+  api: {
+    bodyParser: false,
+  },
+};
 
-const uploadItems = async (req, res) => {
-    if (req.method !== "POST") {
-        return new Response(JSON.stringify({ success: false, message: "Method not allowed" }), {
-            status: 405,
-            headers: { 'Content-Type': 'application/json' }
-        });
-    }
+const uploadItems = async (req,res)=>{
+    if(req.method === 'POST'){
 
-    try {
-        // Ensure the uploads directory exists
-        const uloadDir = "./public/uploads";
-        if (!fs.existsSync(uloadDir)) {
-            fs.mkdirSync(uloadDir, { recursive: true });
-        };
+        try {
 
-        // Use multer to handle the file upload
-        await new promise((resolve, rejects) => {
-            upload.single("image")(req, res, (error) => {
-                if (error) {
-                    rejects(error)
+            const uploadDir = "./public/uploads";
+            if(!fs.existsSync(uploadDir)){
+                fs.mkdirSync(uploadDir, {recursive:true});
+            };
+
+            upload.single("image")(req, res, async (error)=>{
+                if(error){
+                    return res.json({success:false, message:"Failed to upload file"});
                 }
-                else {
-                    resolve()
-                }
-            })
-        });
-
-        const file = req.file;
-
-        if (!file) {
-            return new Response(JSON.stringify({ success: false, message: "No file uploaded" }), {
-                status: 400,
-                headers: { 'Content-Type': 'application/json' }
             });
-        };
 
-        const formData = new FormData();
-        const name = formData.get("name");
-        const price = formData.get("price");
-        const category = formData.get("category");
+            const file = req.file;
+            if(!file){
+                return res.json({success:false, message:"No file uploaded"});
+            }
 
-        if (!name || !category || !price) {
-            return new Response(JSON.stringify({ success: false, message: "All fields required" }), {
-                status: 400,
-                headers: { 'Content-Type': 'application/json' }
+            const {name, price, category} = JSON.parse(req.body.data);
+
+           await connectDB();
+
+            const newItem = new ItemModel({
+                name,
+                price,
+                category,
+                image:file.filename
             });
+
+            await newItem.save();
+
+            return res.json({success:true, newItem, message:"File uploaded successfully"});
+            
+        } catch (error) {
+           console.log("ERROR:", error);
+           return res.json({success:false, message:"Upload file error"});
         }
-
-        await connectDB();
-
-        const newItem = new ItemModel({
-            name,
-            category,
-            price: parseFloat(price),
-            description,
-            image: file.filename
-        });
-
-        await newItem.save();
-
-        return new Response(JSON.stringify({ success: true, newItem, message: "Item added successfully" }), {
-            status: 200,
-            headers: { 'Content-Type': 'application/json' }
-        });
-
-
-    } catch (error) {
-        console.log("ERROR:", error);
-        return new Response(JSON.stringify({ success: false, message: "UploadItem Error" }), {
-            status: 500,
-            headers: { 'Content-Type': 'application/json' }
-        });
-
     }
 }
 
