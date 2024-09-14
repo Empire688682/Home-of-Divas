5572fef3fdc4d0d03a2d3f1fedc717c37f5a99dd
Empire688682 <asehindej@gmail.com>
2024-09-14 01:28:53 +0100

    product

--- home-of-divas/src/Component/AdminCom/ProductList/ProductList.jsx
+++ home-of-divas/src/Component/AdminCom/ProductList/ProductList.jsx
@@ -2,16 +2,16 @@
 
 import React, { useState } from 'react';
 import styles from './ProductList.module.css'; // Converted CSS to module
-import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Image from 'next/image';
+import { useGlobalContext } from '@/Component/Context';
 
 const ProductList = () => {
-  const [allFood, setAllFood] = useState([]);
+  const { allProduct, loading } = useGlobalContext()
 
   return (
     <div className={styles.list_Item}>
-      <h2>All Food List</h2>
+      <h2>All Product List</h2>
       <div className={styles.header}>
         <div className={styles.image}>Image</div>
         <div className={styles.name}>Name</div>
@@ -19,23 +19,32 @@ const ProductList = () => {
         <div className={styles.price}>Price</div>
         <div className={styles.remove}>Remove</div>
       </div>
-              <div className={styles.content}>
-                <div className={styles.img_Con}>
-                  <Image
-                    src='/p1_product_i1.png'
-                    alt=''
-                    fill
-                  />
+      {
+        loading ? <div className={styles.loading}><p>Loading.....</p></div>
+          : <div>
+            {
+              allProduct && allProduct.map((product) => {
+                return <div className={styles.content}>
+                  <div className={styles.img_Con}>
+                    <Image
+                      src={`/uploads/${product.image}`}
+                      alt=''
+                      fill
+                    />
+                  </div>
+                  <div className={styles.name}>{product.name}</div>
+                  <div className={styles.category}>{product.category}</div>
+                  <div className={styles.price}>{product.price}</div>
+                  <div
+                    className={styles.remove}
+                  >
+                    X
+                  </div>
                 </div>
-                <div className={styles.name}>food name</div>
-                <div className={styles.category}>food category</div>
-                <div className={styles.price}>food price</div>
-                <div
-                  className={styles.remove}
-                >
-                  X
-                </div>
-              </div>
+              })
+            }
+          </div>
+      }
     </div>
   );
 };
