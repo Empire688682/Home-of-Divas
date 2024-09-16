37bc055063b9f38a43c701bd41daced4309f3939
Empire688682 <asehindej@gmail.com>
2024-09-16 07:29:21 +0100

    about

---
+++ home-of-divas/src/app/about/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './about.module.css'
+import AboutCom from '@/Component/AboutCom/AboutCom';
+
+const page = () => {
+  return (
+    <div>
+      <AboutCom/>
+    </div>
+  )
+}
+
+export default page
