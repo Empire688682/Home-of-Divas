8bb4248f7dfe8c42c5089b2391daf2a9e898e190
Empire688682 <asehindej@gmail.com>
2024-09-16 21:04:20 +0100

    error

--- home-of-divas/src/helper/getUserToken.js
+++ home-of-divas/src/helper/getUserToken.js
@@ -2,21 +2,10 @@ import jwt from 'jsonwebtoken';
 
 export const userToken = (req) => {
     try {
-        // Extract the 'Authorization' header
-        const authHeader = req.headers.get('authorization');
-        if (!authHeader || !authHeader.startsWith('Bearer ')) {
-            console.log("No token or wrong format in authorization header");
-            return null;
-        }
-
-        // Extract the token from the 'Bearer' format
-        const token = authHeader.split(' ')[1];
+        const token = req.cookies.get('DCToken')?. value || null;
         if (!token) {
-            console.log("Token not found");
             return null;
         }
-
-        // Verify the token
         const decodedToken = jwt.verify(token, process.env.TOKEN_KEY);
         return decodedToken.id; // Make sure your token has 'id' or '_id'
     } catch (error) {
