b4218228266fdd8bce8e103682f6284b86ec64ab
Empire688682 <asehindej@gmail.com>
2024-09-26 02:01:14 +0100

    userHistory bug

--- home-of-divas/src/app/api/order/placeOrder/route.js
+++ home-of-divas/src/app/api/order/placeOrder/route.js
@@ -16,27 +16,23 @@ export async function POST(req) {
         const { itemData } = reqBody;
         const { addressData, item, paymentMethod, total } = itemData;
 
-        // Get user ID from token
         const userId = await userToken(req);
         if (!userId) {
             await session.abortTransaction();
             return NextResponse.json({ success: false, message: 'User not authenticated' });
         }
 
-        // Find user
         const user = await UserModel.findById(userId).session(session);
         if (!user) {
             await session.abortTransaction();
             return NextResponse.json({ success: false, message: 'User not found' });
         }
 
-        // Ensure all required fields are present
         if (!addressData || !item || !paymentMethod || !total) {
             await session.abortTransaction();
             return NextResponse.json({ success: false, message: 'All fields required' });
         }
 
-        // Create a new order
         const newOrder = new OrderModel({
             userId: user._id,
             addressData,
@@ -45,10 +41,9 @@ export async function POST(req) {
             total
         });
 
-        // Save order with the current session
         await newOrder.save({ session });
 
-        // Add order to user's order history and order data
+        // Update user order history and data
         let userOrderHistory = user.userOrderHistory || {};
         let userOrderData = user.userOrderData || {};
 
@@ -60,22 +55,22 @@ export async function POST(req) {
             userOrderData[newOrder._id] = true;
         }
 
-        // Update user's order data and history
         user.userOrderHistory = userOrderHistory;
         user.userOrderData = userOrderData;
 
-        // Save user with the current session
-        await user.save({ session });
+        const updatedUser = await user.save({ session });
+        if (!updatedUser) {
+            console.error("User save operation failed", user);
+        } else {
+            console.log("User updated successfully:", updatedUser);
+        }
 
-        // Commit the transaction only if no errors occurred
         await session.commitTransaction();
         return NextResponse.json({ success: true, data: newOrder, message: 'Order placed successfully' });
     } catch (error) {
-        // If any error occurs, abort the transaction
         await session.abortTransaction();
         return NextResponse.json({ success: false, message: 'Error placing order', error: error.message });
     } finally {
-        // End the session regardless of success or failure
         session.endSession();
     }
 }
