bb1d562975592bcffede2ff884b2f87191d7536b
Empire688682 <asehindej@gmail.com>
2024-09-30 00:56:28 +0100

    admin order

--- home-of-divas/src/Component/AdminCom/Orders/Order.jsx
+++ home-of-divas/src/Component/AdminCom/Orders/Order.jsx
@@ -14,14 +14,12 @@ const Order = () => {
     try {
       setLoading(true);
       const response = await axios.get("/api/order/allOrder");
-      console.log('RESPONSE:', response);
       if (response.data.success) {
         setAllOrder(response.data.data || []);
       }
     } catch (error) {
       console.log("Error:", error);
-    }
-    finally {
+    } finally {
       setLoading(false);
     }
   };
@@ -30,19 +28,34 @@ const Order = () => {
     fetchOrder();
   }, []);
 
-  const removeOrder = async(orderId)=>{
+  const removeOrder = async (orderId) => {
     try {
-      const response = axios.post("/api/order/removeOrder", {orderId});
-      console.log("SUCCESSMESSAGEOute:",response )
-      if((await response).data.success){
-        console.log("SUCCESSMESSAGE:",response )
+      const response = await axios.post("/api/order/removeOrder", { orderId });
+      if (response.data.success) {
         fetchOrder();
       }
     } catch (error) {
-      
+      console.log("Error:", error);
     }
-  }
+  };
 
+  const handleOnChange = (event, orderId) => {
+    const { value } = event.target;
+    updateStatus(orderId, value);
+  };
+
+  const updateStatus = async (orderId, stage) => {
+    try {
+      const response = await axios.post("/api/order/orderStatus", { orderId, stage });
+      if (response.data.success) {
+        fetchOrder(); // Fetch orders again to get updated statuses
+        toast.success("Status updated successfully!");
+      }
+    } catch (error) {
+      console.log("ERROR:", error);
+      toast.error("Failed to update status.");
+    }
+  };
 
   return (
     <div className={styles.order_items}>
@@ -56,8 +69,8 @@ const Order = () => {
                 <div>
                   <div className={styles.header_item}>Item</div>
                   {
-                    order.item.map((item, index) => (
-                      <div key={index} className={styles.item}>
+                    order.item.map((item) => (
+                      <div className={styles.item} key={item.name}>
                         <small>Name: {item.name}</small><br />
                         <small>Qty: {item.quantity}</small>
                       </div>
@@ -68,19 +81,34 @@ const Order = () => {
                   <div>
                     <div className={styles.header_address}>Shipping Address</div>
                     {
-                      Object.keys(order.addressData).length > 0 && <>
-                        <div className={styles.address}>
-                          <small>Name: {order.addressData.firstName} {order.addressData.lastName}</small><br />
-                          <small>City: {order.addressData.city}</small><br />
-                          <small>Street: {order.addressData.street}</small><br />
-                          <small>State: {order.addressData.state}</small><br />
-                          <small>Email: {order.addressData.email}</small><br />
-                          <small>Phone: {order.addressData.phone}</small><br />
-                          <small>ZipCode: {order.addressData.zipCode}</small><br />
-                        </div>
-                      </>
+                      Object.keys(order.addressData).length > 0 && <div className={styles.address}>
+                        <small>Name: {order.addressData.firstName} {order.addressData.lastName}</small><br />
+                        <small>City: {order.addressData.city}</small><br />
+                        <small>Street: {order.addressData.street}</small><br />
+                        <small>State: {order.addressData.state}</small><br />
+                        <small>Email: {order.addressData.email}</small><br />
+                        <small>Phone: {order.addressData.phone}</small><br />
+                        <small>ZipCode: {order.addressData.zipCode}</small><br />
+                      </div>
                     }
                   </div>
+                </div>
+                <div>
+                  <div className={styles.header_amount}>Payment Method</div>
+                  <div className={styles.amount}>{order.paymentMethod}</div>
+                </div>
+                <div>
+                  <div className={styles.header_amount}>Status</div>
+                  <select 
+                    onChange={(event) => handleOnChange(event, order._id)} 
+                    name="stage" 
+                    value={order.status} // Use the current order status
+                    id=""
+                  >
+                    <option value="Processing">Processing</option>
+                    <option value="Delivered">Delivered</option>
+                    <option value="Cancel">Cancel</option>
+                  </select>
                 </div>
                 <div>
                   <div className={styles.header_amount}>Total Amount</div>
@@ -90,7 +118,7 @@ const Order = () => {
                   <div className={styles.header_remove}>Remove</div>
                   <div
                     className={styles.remove}
-                    onClick={()=> removeOrder(order._id)}
+                    onClick={() => removeOrder(order._id)}
                   >
                     X
                   </div>
@@ -99,11 +127,11 @@ const Order = () => {
             ))
           }
         </>
-          :
-          <>{
-            loading ? <div className={styles.loading}><h2>Loading.....</h2></div> : <p>No Order Found</p>
-          }
-          </>
+        :
+        <>{
+          loading ? <div className={styles.loading}><h2>Loading.....</h2></div> : <p>No Order Found</p>
+        }
+        </>
       }
     </div>
   );
---
+++ home-of-divas/src/app/api/order/orderStatus/route.js
@@ -0,0 +1,25 @@
+import { connectDB } from "@/ConnectDB/ConnectDB";
+import { OrderModel } from "@/model/orderModel";
+import { NextResponse } from "next/server";
+connectDB();
+
+export async function POST(req) {
+    const {orderId, stage} = await req.json();
+    console.log("STAGE:", stage);
+    try {
+        if(!orderId || !stage){
+            return NextResponse.json({success:false, message:"All field required"}, {status:400});
+        };
+        const order = await OrderModel.findById({_id:orderId});
+        if(!order){
+            return NextResponse.json({success:false, message:"No order found"}, {status:400});
+        };
+
+        await OrderModel.findByIdAndUpdate(orderId, {status:stage}, {new:true});
+
+        return NextResponse.json({success:true, data:order.status, message:"Status updated"});
+    } catch (error) {
+        console.log("ERROR:", error);
+        return NextResponse.json({success:false, message:"ERROR"}, {status:500});
+    }
+}
